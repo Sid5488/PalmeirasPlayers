77b4ce57460f732e01b9e59f2f6de165ce2e3e11
Guilherme Sousa <guilhermenoobsaibot@gmail.com>
2020-08-31 03:21:31 -0300

    Migrations

---
+++ Data/Migrations/20200831060431_tb_players.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PalmeirasPlayers.Migrations
+{
+    public partial class tb_players : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 60, nullable: false),
+                    PlayingPosition = table.Column<string>(maxLength: 60, nullable: false),
+                    ShirtNumber = table.Column<int>(nullable: false),
+                    Age = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_players", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_players");
+        }
+    }
+}
